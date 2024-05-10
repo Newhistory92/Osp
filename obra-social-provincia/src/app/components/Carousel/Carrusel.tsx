df129f67ccd86f694455a32373fa913d491c3f9e
Newhistory92 <federojo28@gmail.com>
2024-05-10 12:55:33 -0300

    10/05

--- obra-social-provincia/src/app/components/Carousel/Carrusel.tsx
+++ obra-social-provincia/src/app/components/Carousel/Carrusel.tsx
@@ -13,25 +13,37 @@ function ControlledCarousel() {
     <Carousel.Item interval={1000}>
       <img src="https://i.pinimg.com/564x/79/69/da/7969da539aa5937622aa1cc9193943d2.jpg" alt={''}  />
       <Carousel.Caption>
-                <h2>Third slide label</h2>
-                <h1>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</h1>
-                <h4></h4>
+      <div className="header-content">
+                        <div className="carousel-line "></div>
+                        <h2>Teimagine Digital Experience with</h2>
+                        <h1>Start-ups and solutions</h1>
+                        <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
+                        
+                    </div>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item interval={500}>
       <img src="https://i.pinimg.com/564x/2a/67/df/2a67df349af5a38ded2018f391421863.jpg" alt={''}  />
       <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div className="header-content">
+                        <div className="carousel-line "></div>
+                        <h2>Teimagine Digital Experience with</h2>
+                        <h1>Start-ups and solutions</h1>
+                        <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
+                        
+                    </div>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
       <img src="https://i.pinimg.com/564x/97/cd/ff/97cdff818907968caffe0bf8b977dc20.jpg" alt={''}  />        
       <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>
-          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-        </p>
+      <div className="header-content">
+                        <div className="carousel-line "></div>
+                        <h2>Teimagine Digital Experience with</h2>
+                        <h1>Start-ups and solutions</h1>
+                        <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
+                        
+                    </div>
       </Carousel.Caption>
     </Carousel.Item>
   </Carousel>
